66d98d5393d66935c6c5c97ef578714615a073c7
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-13 08:41:38 -0500

    Create forloopforunderspecificnumber_const.go

---
+++ forloopforunderspecificnumber_const.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  const stopat = 500
+  startat := 20
+  const add = 10
+  for startat < stopat {
+    startat += add
+  if startat < stopat {
+    fmt.Printf("Your number is under the number of stopat. \n Your stopat number: %d \n Number currently: %d \n", stopat, startat)
+  } else {
+    fmt.Printf("Done! At the number of %d", stopat)
+  }
+    }
+  }
